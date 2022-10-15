1d54bc8fa568dea844b8a5ff1c1166cae107c99a
Iain Penman <46635364+imp67@users.noreply.github.com>
2022-10-15 19:33:17 +1100

    First set of mods config Pro to Std

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -82,7 +82,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(IMP, ADStd-BIQU-UART-BLT)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(IMP, ADStd-BIQU-noUART-BLT)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
