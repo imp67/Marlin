4977374b1a09a7b926522b768027ded23dcb7056
Iain Penman <46635364+imp67@users.noreply.github.com>
2022-10-27 16:56:16 +1100

    Standardise printer name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -163,7 +163,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "A-D Pro Imp M21xBF Vt Town"
+#define CUSTOM_MACHINE_NAME "A-D Pro Imp M21xBF UART BLTzmin"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
